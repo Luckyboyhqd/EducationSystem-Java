7f23f53433e8348820726eff9ef74551e6b8e2d1
Luckyboyhqd <33447010+Luckyboyhqd@users.noreply.github.com>
2021-05-25 18:15:53 +0800

    Create REDEME.m

---
+++ REDEME.m
@@ -0,0 +1,1 @@
+The education system is a convenient system for teachers and students to arrange the curriculum, the teacher arranges the class on the system, and the students log in the system to view their courses in real time. It is very convenient for course management of teachers and students in school.
